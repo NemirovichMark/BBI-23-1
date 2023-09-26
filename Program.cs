4e09f4998728268dc78dcf36c3f7082f2f184a58
TaranovArtem <146069426+TaranovArtem@users.noreply.github.com>
2023-09-26 12:03:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace laba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.1
+            Console.WriteLine(123);
+            #endregion
+            //
+            #region
+
+            #endregion
+        }
+    }
+}
