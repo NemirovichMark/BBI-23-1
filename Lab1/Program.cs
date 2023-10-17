5b126fdaff5888428c32307b682f8cfe5476d55c
VorobevIvan <146070269+VorobevIvan@users.noreply.github.com>
2023-10-17 11:13:41 +0300

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace project
+{
+    class project
+    {
+        static void Main()
+        {
+
+            double o = 1;
+            double a = 0.1;
+            double h = 0.1;
+            for (double x = a; x <= o; x += h)
+            {
+                double k = x * x * x;
+                double s = 0;
+                int i = 1;
+                while (Math.Abs(((2 * (i % 2)) - 1) * (k / (4 * i * i - 1))) >= 0.0001)
+                {
+                    s += (2 * (i % 2) - 1) * (k / (4 * i * i - 1));
+                    k *= x * x;
+                    i += 1;
+                }
+                double y = (1 + x * x) * Math.Atan(x) / 2 - x / 2;
+                Console.WriteLine($"{s} {y}");
+            }
+        }
+
+
+    }
+}
