48e32f54bb273646009736d85f6c8ee64f2afac5
SofiaGubeeva <146069686+SofiaGubeeva@users.noreply.github.com>
2023-10-17 11:40:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more informati
+using System;
+
+//double sum = 0;
+//for(double i = 4; i <= 21; i++)
+//{
+//    sum += (i - 2) / i;
+//}
+//Console.WriteLine(sum);
+//Console.ReadKey();
+
+//double s = 0;
+//int x = 4;
+//int n = 1;
+//double c;
+//do
+//{
+//    c = 1 / n * Math.Pow(x, n);
+//    s += c;
+//    n += 1;
+//} while (c >= 0.0001);
+//Console.WriteLine();
+
+//Console.WriteLine(s);
+//double sum = 1;
+//int x = 2;
+//int power = 2;
+//double temp = 0;
+
+//do
+//{
+//    temp = 1 / Math.Pow(x, power);
+//    sum += temp;
+//    power += 2;
+//} while (temp >= 0.0001);
+//Console.WriteLine(sum);
+//Console.ReadKey();
+
+//for (double x = 0; x <= Math.PI; x += (Math.PI / 10))
+//{
+//    double y = x + Math.Cos(2 * x - x);
+//    Console.WriteLine($"{x} {y}");
+//}
+//double p = 5;
+//double n = 1;
+
+//do
+//{
+//    p = p * n;
+//    if (p > 600000) ;
+//    n += 2;
+
+//} while (p <= 600000); 
+//Console.WriteLine(n);
+//Console.ReadKey();
+//do
+//{
+//    double s = 0;
+//    double k = 2 * 2;
+//    for(int i = 1; i 
+
+//}
+//double s = 0;
+//double x = 4;
+//double i = 1;
+//while (1 / (i * Math.Pow(x, i)) >= 0.0001)
+//{
+//    s += 1 / (i * Math.Pow(x, i));
+//    i++;
+//}
+//Console.WriteLine(s);
+
+//double s = 0;
+//double a = 2 * 2;
+////for(int i = 1; i <= 14; i += 2)
+////{
+////    s += a;
+////    a *= 2 * 2;
+////    a = -a;
+////}
+
+//double s = 0;
+//double a =1;
+//for (int i = 0; i <= 14; i += 2)
+//{
+//    s +=a;
+//    a*= 2 * 2;
+//    a = -a;
+//}
+//Console.WriteLine(s);
