872e782a5a30ea8107d8d00a8ed6d86b2e74d07d
AgapovaEkaterina <146069418+AgapovaEkaterina@users.noreply.github.com>
2023-10-31 00:38:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,391 +1,155 @@
 ﻿
 using System;
-
 class Program
 {
-    static void Main()
-
+    
+    public static void Main(string[] args)
     {
-        #region 1_1 
-        //int s = 0;
-        //for (int i = 2; i <+ 35; i += 3)
-        //{
-        //    s += i;
-        //}
-        //Console.WriteLine(s);
-        //Console.ReadLine();
+        //в списке номер 2
+        #region 1-3
+        Console.WriteLine("1-3");
+        double a, b, c;
+        Console.WriteLine("Введите a:");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите b:");
+        b = double.Parse(Console.ReadLine());
+        if (a > 0)
+        {
+            c = Math.Max(a, b);
+        }
+        else
+        {
+            c = Math.Min(a, b);
+        }
+        Console.WriteLine("Ответ:");
+        Console.WriteLine(c);
+        Console.WriteLine();
         #endregion
 
-        #region 1_2 
-        //double s = 0;
-        //for (double i = 1; i < 11; i++)
-        //{
-        //    s += 1 / i;
-        //}
-        //Console.WriteLine(s);
-        //Console.ReadLine();
+        #region 1-6
+        Console.WriteLine("1-6");
+        double pi = Math.PI;
+        double r, s;
+        //1
+        r = 3.2;
+        s = 3.5;
+        Console.WriteLine("Ответы:");
+        Console.WriteLine("3.1");
+        if (Math.Sqrt(s) / 2 >= Math.Sqrt(r / pi))
+        {
+            Console.WriteLine("Можно");
+        }
+        else
+        {
+            Console.WriteLine("Нелья");
+        }
+        r = 3.2;
+        s = 4;
+        Console.WriteLine("3.2");
+        if (Math.Sqrt(s) / 2 >= Math.Sqrt(r / pi))
+        {
+            Console.WriteLine("Можно");
+        }
+        else
+        {
+            Console.WriteLine("Нелья");
+        }
+        r = 6;
+        s = 9;
+        Console.WriteLine("3.3");
+        if (Math.Sqrt(s) / 2 >= Math.Sqrt(r / pi))
+        {
+            Console.WriteLine("Можно");
+        }
+        else
+        {
+            Console.WriteLine("Нелья");
+        }
+        Console.WriteLine();
         #endregion
 
-        #region 1_3
-        //double s = 0;
-        //for (double i = 2; i < 113; i += 2)
-        //{
-        //    s += i / (i + 1);
-        //}
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-
+        #region 1-9
+        Console.WriteLine("1-9");
+        double y = 0;
+        Console.WriteLine("Введите x,чтобы посчитать y:");
+        double x = double.Parse(Console.ReadLine());
+        if (x <= -1)
+        {
+            y = 0;
+        }
+        if (x > -1 && x <= 0)
+        {
+            y = x + 1;
+        }
+        if (x > 0)
+        {
+            y = 1;
+        }
+        Console.WriteLine("Значение функции:");
+        Console.WriteLine(y);
+        Console.WriteLine();
         #endregion
 
-        #region 1_4
-        //double s = 0;
-        //double x = 1;
-        //double st = x;
-
-        //for (double n = 1; n <= 9; n++)
-        //{
-        //    s += Math.Cos(n * x) / st;
-        //    st *= x;
-        //}
-
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-        #endregion
-        #region 1_5
-        //double p = 30;
-        //double h = 32;
-        //double s = 0;
-        //for (double i = 0; i < 10; i++)
-        //{
-        //    s += Math.Pow((p + i * h), 2);
-        //}
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-
-        #endregion
-        #region 1_6
-        //double y = 0;
-        //double x;
-        //for (x = -4; x <= 4; x += 0.5)
-        //{
-        //    y = 0.5 * Math.Pow(x, 2) - 7 * x;
-        //    Console.WriteLine($"{x},{y}");
-        //    Console.ReadLine();
-        //}
-
+        #region 2-3
+        Console.WriteLine("2-3");
+        Console.WriteLine("Введите число учеников:");
+        double n = double.Parse(Console.ReadLine());
+        double w;
+        double temp = 0;
+        Console.WriteLine("Введите веc учеников:");
+        for (double i = 0; i < n; i++)
+        {
+            w = double.Parse(Console.ReadLine());
+            if (w < 30)
+            {
+                temp += 1;
+            }
+        }
+        temp *= 0.2;
+        temp = Math.Round(temp, 1);
+        Console.WriteLine("Кол-во литров:");
+        Console.WriteLine(temp);
+        Console.WriteLine();
         #endregion
 
-        #region 1_7
-        //int s = 1;
-        //for (int i = 1; i <= 6; i++)
-        //{
-        //    s *= i;
-        //}
-        //s = s * 6;
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-        #endregion
-        #region 1_8
-        //int n = 6;
-        //int s = 0;
-        //int factorial = 1;
-
-        //for (int i = 1; i <= n; i++)
-        //{
-        //    factorial *= i;
-        //    s += factorial;
-        //}
-
-        //Console.WriteLine(s);
-        //Console.ReadLine();
+        #region 2-6
+        Console.WriteLine("2-6");
+        double X, Y = 0;
+        temp = 0;
+        Console.WriteLine("Введите кол-во координат");
+        n = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите координаты x и y");
+        for (double i = 0; i < n; i++)
+        {
+            X = double.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine());
+            if (X >= 0 && X <= pi && Y <= Math.Sin(X))
+            {
+                temp += 1;
+            }
+        }
+        Console.WriteLine("Кол-во координат,которые входят:");
+        Console.WriteLine(temp);
+        Console.WriteLine();
         #endregion
 
-        #region 1_9
-        //int n = 6;
-        //double s = 0;
+        #region 2-9
+        {
+            Console.WriteLine("2-9");
+            Console.WriteLine("Введите кол-во участников:");
+            n = double.Parse(Console.ReadLine());
+            double max = -1000000;
+            temp = 0;
+            Console.WriteLine("Введите результаты участников:");
+            for (double i = 0; i < n; i++)
+            {
+                temp = double.Parse(Console.ReadLine());
+                max = Math.Max(max, temp);
+            }
+            Console.WriteLine("Лучший результат:");
+            Console.WriteLine(max);
+            #endregion
 
-        //for (int i = 1; i <= n; i++)
-
-        //{
-        //    n = i * i;
-        //    s += Math.Pow(-1, i) * Math.Pow(5, i) / fact(i);
-        //}
-
-        //static long fact(int n)
-        //{
-        //    long result = 1;
-        //    for (int i = 1; i <= n; i++)
-        //    {
-        //        result *= i;
-        //    }
-
-        //    return result;
-        //}
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-        #endregion
-
-        #region 1_10
-        //int a = 3;
-        //int step = 7;
-        //int res = 1;
-        //for (int i = 0; i < step; i++)
-        //{
-        //    res *= a;
-        //}
-        //Console.WriteLine(res);
-        #endregion
-
-        #region 1_11
-        //for(int i = 1; i <= 6; i++)
-        //    {
-        //    Console.WriteLine(i);
-        //}
-
-        //for (int i = 1; i <= 6; i++)
-        //{
-        //    Console.WriteLine(5);
-        //    Console.ReadLine();
-        //}
-
-        #endregion
-
-        #region 1_12
-        //double s = 1;
-        //double x = 7;
-        //double k = x;
-
-        //for (int i = 1; i <= 10; i++)
-        //{
-        //    s = s + 1 / k;
-        //    k *= x;
-        //}
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-
-        #endregion
-
-        #region 1_13
-        //double x, y;
-        //double h = 0.1;
-        //for (x = -1.5; x<= 1.5; x+=h)
-        //{
-        //    if (x <= -1) y = 1;
-        //    if (x <= 1 & x>-1) y = -x;
-        //    else y = -1;
-        //    Console.WriteLine($"{x} {y}");
-        //}
-        //Console.WriteLine();
-        //Console.ReadLine();
-        #endregion
-        #region 1_14
-        //int n = 8;
-        //int a = 1;
-        //int b = 1;
-
-        //Console.Write(a + " " + b + " ");
-
-        //for (int i = 3; i <= n; i++)
-        //{
-        //    int c = a + b;
-        //    Console.Write(c + " ");
-        //    a = b;
-        //    b = c;
-        //}
-
-        //Console.WriteLine();
-        //Console.ReadLine();
-
-        #endregion
-
-        #region 1_15
-        //double a1 = 1;
-        //double b1 = 1;
-        //double a2 = 2;
-        //double b2 = 1;
-        //int n = 5;
-        //for (int i = 3; i <= n; i++)
-        //{
-        //    double nexta = a1 + a2;
-        //    double nextb = b1 + b2;
-        //    a1 = a2;
-        //    b1 = b2;
-        //    a2 = nexta;
-        //    b2 = nextb;
-
-        //}
-        //double res = a2 / b2;
-        //Console.WriteLine(res);
-        //Console.ReadLine();
-
-        #endregion
-
-        #region 2_1
-        //double x = 1.0;
-        //const double Eps = 0.0001;
-        //double sum = 0.0, element;
-        //int n = 1;
-        //do
-        //{
-        //    element = Math.Cos(n * x) / Math.Pow(n, 2);
-        //    sum += element;
-        //    n += 1;
-        //} while (Math.Abs(element) > Eps);
-        //Console.WriteLine( sum);
-        //Console.ReadLine();
-
-        #endregion
-
-        #region 2_2
-        //int n = 1;
-        //int p = 1;
-
-        //for (int i = n; i = i + 3);
-        //{
-        //    if (p * i < 30000)
-        //        p = p * i;
-        //    else
-        //    {
-        //        n = i - 3;
-        //        break;
-        //    }
-        //}
-        //Console.WriteLine(n);
-        //Console.ReadLine();
-        #endregion
-        #region 2_3
-        //double sum = 0;
-        //const double p = 15;
-        //int n = 0;
-        //double a = 2;
-        //double h = 4;
-
-        //while (sum + a <= p)
-        //{
-        //    n++;
-        //    sum += a;
-        //    a += h;
-        //}
-
-        //Console.WriteLine(n);
-        //Console.ReadLine();
-        #endregion
-        #region 2_4
-        //const double eps = 0.0001;
-        //double x = 0.9;
-        //double s = 1;
-        //double n = 0;
-        //double a = 1;
-
-        //while (a >= eps)
-
-        //{
-        //    n++;
-        //    a *= x * x;
-        //    s += a;
-        //}
-
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-
-        #endregion
-        #region 2_5
-        //int N, M;
-        //int res = 5;
-        //N = 5;
-        //M = 3;
-        //while (N >= M)
-        //{
-        //    N -= M;
-        //    res++;
-        //}
-        //Console.WriteLine($"частное: {res}, остаток: {N}");
-        //Console.ReadKey();
-        //Console.ReadLine();
-        #endregion
-
-        #region 2_7(a)
-        //double s = 10;
-        //double u = 0;
-        //double k = 0;
-        //while (k < 7)
-        //{
-        //    u += s;
-        //    s = s * 1.1;
-        //    k += 1;
-        //}
-        //Console.WriteLine(u);
-        //Console.WriteLine();
-        //Console.ReadLine();
-        #endregion
-        #region 2_7(б)
-        //double s = 10;
-        //double u = 0;
-        //double k = 0;
-        //while (u < 100)
-        //{
-        //    u += s;
-        //    s *= 1.1;
-        //    k++;
-        //}
-        //Console.WriteLine(k);
-        //Console.WriteLine();
-        //Console.ReadLine();
-        #endregion
-        #region 2_7(в)
-        //double s = 10;
-        //double k = 1;
-        //while (s < 20)
-        //{
-        //    s *= 1.1;
-        //    k++;
-        //}
-        //Console.WriteLine(k);
-        //Console.WriteLine();
-        //Console.ReadLine();
-        #endregion
-        #region 2_8
-        //int s = 10000;
-        //int n = 0;
-        //while (s < 20000)
-        //{
-        //    s += s * 8 / 100;
-        //    n++;
-        //}
-        //Console.WriteLine(n);
-        //Console.ReadLine();
-        #endregion
-        #region 3_2
-        //double s = 0;
-        //double eps = 0.0001;
-        //double a = 0.1;
-        //double b = 0.8;
-        //double h = 0.1;
-        //double x = a;
-        //int i = 0;
-        //double y;
-        //while (true)
-        //{
-        //    double now = (Math.Pow(x, i) * Math.Sin((i * Math.PI) / 4));
-
-        //    if (now < eps)
-        //    {
-        //        y = ((x * Math.Sin((Math.PI / 4))) / (1 - 2 * x * Math.Cos((Math.PI) / 4) + x * x));
-        //        break;
-        //    }
-        //    else
-        //    {
-        //        s += now;
-        //    }
-        //    i++;
-        //    x += h;
-        //}
-        //Console.WriteLine(y);
-        //Console.WriteLine(s);
-        //Console.ReadLine();
-        #endregion
-
+        }
     }
-
-
 }
