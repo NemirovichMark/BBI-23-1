03c9d1afe2bfe79fa2eb03f9d8ff1537e83b5a4a
KarinaAbramenkova <146069361+KarinaAbramenkova@users.noreply.github.com>
2023-10-30 23:07:40 +0300

    Add files via upload

---
+++ лр 1,3.cs
@@ -0,0 +1,39 @@
+﻿{
+    static long Fact(long n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Fact(n - 1);
+    }
+    {
+        double s = 0;
+        double a = 0.1;
+        double b = 1;
+        double h = 0.1;
+        int i = 0;
+        double z = 1;
+        for (double x = a; x <= b; x += h)
+        {
+            s = 0;
+            i = 0;
+            z = 1;
+            double k = z * MyPow(x, 2 * i) / Fact(2 * i);
+            while (Math.Abs(k) >= 0.0001)
+            {
+                k = z * MyPow(x, 2 * i) / Fact(2 * i);
+                s += k;
+                i += 1;
+                z = -z;
+            }
+            double y = Math.Cos(x);
+            Console.WriteLine($"{s} {y}");
+        }
+
+    }
+}
+
+double MyPow(double x, int v)
+{
+    throw new NotImplementedException();
+}
