48facf477285f26e5b5f86d25350a47f9fcdb274
StotsenkoAlexandr <146069394+StotsenkoAlexandr@users.noreply.github.com>
2023-11-28 11:59:20 +0300

    Update Lab4.cs

--- Lab4.cs
+++ Lab4.cs
@@ -749,6 +749,85 @@ namespace project
             }
             #endregion
             Console.WriteLine();
+
+
+            #region 3_1 
+            Console.WriteLine("3_1:");
+            n = 7;
+            m = 5;
+            s = 0;
+            I = -1;
+            I1 = 0;
+            J = -1;
+            arm = 0;
+            q = 0;
+            arr1 = new double[7];
+            arr2 = new double[7];
+            arr3 = new double[7];
+            mtrx = new double[n, m];
+            mtrx1 = new double[n - 1, m];
+            max = -Math.Pow(10, 20);
+            min = Math.Pow(10, 20);
+            min1 = Math.Pow(10, 20);
+            Console.WriteLine("Введите матрицу(Кол-во строк: " + n + "; Кол-во элементов в строке: " + m + ")");
+            for (int i = 0; i < n; i++)
+            {
+                str = Console.ReadLine();
+                double[] arr = str.Split(' ').Select(double.Parse).ToArray();
+                for (int j = 0; j < m; j++)
+                {
+                    mtrx[i, j] = arr[j];
+                }
+            }
+            Console.WriteLine("Исходная матрица:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(mtrx[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mtrx[i,j] <min)
+                    {
+                        arr1[i] = mtrx[i, j];
+                        min = mtrx[i,j];
+                    }
+                }
+            }
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n-1; j++)
+                {
+                    if (arr1[j] < arr1[j + 1])
+                    {
+                        q = arr1[j];
+                        arr1[j] = arr1[j + 1];
+                        arr1[j + 1] = q;
+                        for (int k = 0; k < m; k++)
+                        {
+                            q = mtrx[j, k];
+                            mtrx[j, k] = mtrx[j + 1, k];
+                            mtrx[j + 1, k] = q;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Итоговая матрица:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(mtrx[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+            Console.WriteLine();
         }
     }
 }
