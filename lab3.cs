3f916c0ec6519893aee28bc06cbd85defc7df0dd
SofiaGubeeva <146069686+SofiaGubeeva@users.noreply.github.com>
2023-12-02 20:38:27 +0300

    Update lab3.cs
    Добрый вечер, исправила ошибки

--- lab3.cs
+++ lab3.cs
@@ -587,9 +587,10 @@ namespace project
 
             #region 2_10
             b = Convert.ToInt32(Console.ReadLine());
-            mas = new double[b];
+            mas  = new double[b];
             p = Math.Pow(10, 20);
             h = 0;
+            bool flag = false;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -600,16 +601,34 @@ namespace project
                     p = mas[i];
                 }
             }
-            for (int i = h + 1; i < mas.Length; i++)
+            foreach (double elem in mas)
             {
-                mas[i - 1] = mas[i];
+                if (elem > 0)
+                {
+                    flag = true;
+                }
             }
-            for (int i = 0; i < mas.Length - 1; i++)
+            if (flag)
             {
-                Console.Write("{0:f1}", mas[i] + " ");
+                for (int i = h + 1; i < mas.Length; i++)
+                {
+                    mas[i - 1] = mas[i];
+                }
+                for (int i = 0; i < mas.Length - 1; i++)
+                {
+                    Console.Write("{0:f1}", mas[i] + " ");
+                }
             }
-            Console.WriteLine();
+            else
+            {
+                foreach (double elem in mas)
+                {
+                    Console.Write(elem + " ");
+                }
+                Console.WriteLine();
+            }
             #endregion
+            Console.WriteLine();
 
             #region 2_11
             p = Convert.ToInt32(Console.ReadLine());
@@ -617,7 +636,7 @@ namespace project
             mas = new double[b];
             mas1 = new double[b + 1];
             h = 0;
-            o = 0;
+            flag = false;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -625,25 +644,42 @@ namespace project
                 if (mas[i] > 0)
                 {
                     h = i;
-                    o = mas[i];
                 }
             }
-            for (int i = 0; i < mas.Length + 1; i++)
+            foreach (double elem in mas)
             {
-                if (i <= h)
+                if (elem > 0)
                 {
-                    mas1[i] = mas[i];
-                }
-                if (i == (h + 1))
-                {
-                    mas         [i] = p;
-                }
-                if (i > (h + 1))
-                {
-                    mas1[i] = mas[i - 1];
+                    flag = true;
                 }
             }
-            Console.WriteLine("[{0}]", string.Join("; ", mas1));
+            if (flag)
+            {
+                for (int i = 0; i < mas.Length + 1; i++)
+                {
+                    if (i <= h)
+                    {
+                        mas1[i] = mas[i];
+                    }
+                    if (i == (h + 1))
+                    {
+                        mas1[i] = p;
+                    }
+                    if (i > (h + 1))
+                    {
+                        mas1[i] = mas[i - 1];
+                    }
+                }
+                Console.WriteLine("[{0}]", string.Join("; ", mas1));
+            }
+            else
+            {
+                foreach (double elem in mas)
+                {
+                    Console.Write(elem + " ");
+                }
+                Console.WriteLine();
+            }
             
             Console.WriteLine();
             #endregion
@@ -651,12 +687,11 @@ namespace project
             #region 2_12
             b = Convert.ToInt32(Console.ReadLine());
             mas = new double[b];
-            mas1 = new double[b + 1];
             h = 0;
             k = 0;
             o = -Math.Pow(10, 20);
-            sum = 0;
-            p = 0;
+            double s = 0;
+            flag = false;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -667,20 +702,38 @@ namespace project
                     o = mas[i];
                 }
             }
-            for (int i = h + 1; i < mas.Length; i++)
+            foreach (double elem in mas)
             {
-                sum += mas[i];
-            }
-            for (int i = 0; i < mas.Length; i++)
-            {
-                if (mas[i] < 0)
+                if (elem < 0)
                 {
-                    k = i;
-                    break;
+                    flag = true;
                 }
             }
-            mas[k] = sum;
-            Console.WriteLine("[{0}]", string.Join("; ", mas));
+            if (flag)
+            {
+                for (int i = h + 1; i < mas.Length; i++)
+                {
+                    s += mas[i];
+                }
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (mas[i] < 0)
+                    {
+                        k = i;
+                        break;
+                    }
+                }
+                mas[k] = s;
+                Console.WriteLine("[{0}]", string.Join("; ", mas));
+            }
+            else
+            {
+                foreach (double elem in mas)
+                {
+                    Console.Write(elem + " ");
+                }
+                Console.WriteLine();
+            }
             Console.WriteLine();
             #endregion
 
@@ -710,6 +763,7 @@ namespace project
             h = 0;
             k = 0;
             o = -Math.Pow(10, 20);
+            flag = false;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -720,57 +774,77 @@ namespace project
                     o = mas[i];
                 }
             }
-            for (int i = 0; i < mas.Length; i++)
+            foreach (double elem in mas)
             {
-                if (mas[i] < 0)
+                if (elem < 0)
                 {
-                    k = i;
-                    break;
+                    flag = true;
                 }
             }
-            mas[h] = mas[k];
-            mas[k] = o;
-            Console.WriteLine("[{0}]", string.Join("; ", mas));
+            if (flag)
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (mas[i] < 0)
+                    {
+                        k = i;
+                        break;
+                    }
+                }
+                mas[h] = mas[k];
+                mas[k] = o;
+                Console.WriteLine("[{0}]", string.Join("; ", mas));
+            }
+            else
+            {
+                foreach (double elem in mas)
+                {
+                    Console.Write(elem + " ");
+                }
+                Console.WriteLine();
+            }
             Console.WriteLine();
             #endregion
 
             #region 2_15
             b = Convert.ToInt32(Console.ReadLine());
-            mas = new double[b];
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
                 mas[i] = a;
             }
-            b = Convert.ToInt32(Console.ReadLine());
-            mas1 = new double[b];
+            int n = int.Parse(Console.ReadLine());
+            mas1 = new double[n];
             for (int i = 0; i < mas1.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
                 mas1[i] = a;
             }
-            int K = Convert.ToInt32(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
             mas2 = new double[mas.Length + mas1.Length];
-            h = 0;
-            k = 0;
-            sum = 0;
-            p = 0;
-            for (int i = 0; i < mas.Length + mas1.Length; i++)
+            h -= 1;
+            if (((h + 1) > mas.Length) || ((h == 1) & (mas.Length == 1)))
             {
-                if (i <= K)
+            }
+            else
+            {
+                for (int i = 0; i < mas.Length + mas1.Length; i++)
                 {
-                    mas2[i] = mas[i];
-                }
-                if (i > K & i <= K + mas1.Length)
-                {
-                    mas2[i] = mas1[i - K - 1];
-                }
-                if (i > K + mas1.Length)
-                {
-                    mas2[i] = mas[i - mas1.Length - 1];
+                    if (i <= h)
+                    {
+                        mas2[i] = mas[i];
+                    }
+                    if (i > h & i <= h + mas1.Length)
+                    {
+                        mas2[i] = mas1[i - h - 1];
+                    }
+                    if (i > (h + mas1.Length))
+                    {
+                        mas2[i] = mas[i - mas1.Length];
+                    }
                 }
             }
-            Console.WriteLine("[{0}]", string.Join("; ", mas2));
+            Console.WriteLine( "[{0}]", string.Join("; ", mas2));
             Console.WriteLine();
             #endregion
 
@@ -780,7 +854,7 @@ namespace project
             h = 0;
             sum = 0;
             sr = 0;
-            K = 0;
+            int K = 0;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -809,53 +883,61 @@ namespace project
             #endregion
 
             #region 2_17
-            b = Convert.ToInt32(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
             mas = new double[b];
-            h = 0;
-            k = 0;
-            sum = 0;
+            int I = 0;
+            int I1 = 0;
+            s = 0;
             sr = 0;
             q = 0;
-            o = Math.Pow(10, 20);
+            double l = Math.Pow(10, 20);
             p = -Math.Pow(10, 20);
+            Console.WriteLine("Введите элементы массива: ");
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
                 mas[i] = a;
-                if (mas[i] < o)
+                if (mas[i] < l)
                 {
-                    h = i;
-                    o = mas[i];
+                    I = i;
+                    l = mas[i];
                 }
                 if (mas[i] > p)
                 {
-                    k = i;
+                    I1 = i;
                     p = mas[i];
                 }
             }
-            if (k < h)
+            if (I1 < I)
             {
                 for (int i = 0; i < mas.Length; i++)
                 {
                     if (mas[i] > 0)
                     {
-                        sum += mas[i];
+                        s += mas[i];
                         q += 1;
                     }
                 }
-                sr = sum / q;
+                sr = s / q;
             }
-            if (k > h)
+            if (I1 > I)
             {
                 for (int i = 0; i < mas.Length; i++)
                 {
                     if (mas[i] < 0)
                     {
-                        sum += mas[i];
+                        s += mas[i];
                         q += 1;
                     }
                 }
-                sr = sum / q;
+                if (q == 0)
+                {
+                    Console.WriteLine("Ответ: 0");
+                }
+                else
+                {
+                    sr = s / q;
+                }
             }
             Console.WriteLine(sr);
             Console.WriteLine();
@@ -989,14 +1071,11 @@ namespace project
             #endregion
 
 
-            #region 3_1
-            b = Convert.ToInt32(Console.ReadLine());
+            #region 3_2
+            b = int.Parse(Console.ReadLine());
             mas = new double[b];
-            h = 0;
-            k = 0;
-            sum = 0;
-            K = 0;
             p = -Math.Pow(10, 20);
+            K = 1;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -1010,26 +1089,20 @@ namespace project
             {
                 if (mas[i] == p)
                 {
+                    mas[i] = mas[i] + K;
                     K++;
                 }
             }
-            mas1 = new double[K];
-            for (int i = 0; i < mas.Length; i++)
-            {
-                if (mas[i] == p)
-                {
-                    mas1[h] = i;
-                    h += 1;
-                }
-            }
-            Console.WriteLine("[{0}]", string.Join(';', mas1));
+            Console.Write("[{0}]", string.Join("; ", mas));
             Console.WriteLine();
             #endregion
 
-            #region 3_2
-            b = Convert.ToInt32(Console.ReadLine());
+            #region 3_3
+            b = int.Parse(Console.ReadLine());
             mas = new double[b];
             p = -Math.Pow(10, 20);
+            I = 0;
+            double kr = 0;
             for (int i = 0; i < mas.Length; i++)
             {
                 double a = Convert.ToDouble(Console.ReadLine());
@@ -1039,15 +1112,17 @@ namespace project
                     p = mas[i];
                 }
             }
-            for (int i = 0; i < mas.Length; i++)
+            if (I % 2 != 0)
             {
-                if (mas[i] == p)
-                {
-                    mas[i] = mas[i] + i;
-                }
+                I -= 1;
             }
-            Console.WriteLine("[{0}]", string.Join(';', mas));
-          
+            for (int i = 0; i < I; i += 2)
+            {
+                kr = mas[i];
+                mas[i] = mas[i + 1];
+                mas[i + 1] = kr;
+            }
+            Console.Write("[{0}]", string.Join("; ", mas));
             Console.WriteLine();
 #endregion
         }
