9de97a24347c763cefef265fd055e55cbc812726
RomanIbramhalilov <146069497+RomanIbramhalilov@users.noreply.github.com>
2023-12-09 17:44:13 +0300

    Add files via upload
    исправленный номер 2.9

--- Program.cs
+++ Program.cs
@@ -484,37 +484,36 @@ for (int i = 0; i < a.GetLength(0); i++)
 
 #region 2.9
 Console.WriteLine("2.9");
- a = new int[6, 7]
-        {
+ matrix = new int[6, 7]
+       {
             {1, 2, 3, 4, 5, 6, 7},
             {8, 9, 10, 11, 12, 13, 14},
             {15, 16, 17, 18, 19, 20, 21},
             {22, 23, 24, 25, 26, 27, 28},
             {29, 30, 31, 32, 33, 34, 35},
-            {36, 37, 38, 39, 40, 41, 42}
-        };
-
-int rf = a.GetLength(0);
-int co = a.GetLength(1);
+            {36, 37, 38, -39, 40, 41, 42}
+       };
 
 
-int[,] obrat = new int[rf, co];
+int ro = matrix.GetLength(0);
+int co = matrix.GetLength(1);
 
-
-for (int i = 0; i < rf; i++)
+for (int j = 0; j < co / 2; j++)
 {
-    for (int j = 0; j < co; j++)
+    for (int i = 0; i < ro; i++)
     {
-        obrat[i, j] = a[rf - i - 1, j];
+        int temp = matrix[i, j];
+        matrix[i, j] = matrix[i, co - 1 - j];
+        matrix[i, co - 1 - j] = temp;
     }
 }
 
-Console.WriteLine("измененная матрица");
-for (int i = 0; i < rf; i++)
+
+for (int i = 0; i < ro; i++)
 {
     for (int j = 0; j < co; j++)
     {
-        Console.Write(obrat[i, j] + " ");
+        Console.Write(matrix[i, j] + " ");
     }
     Console.WriteLine();
 }
