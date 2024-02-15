b8635d4e55f56b53e48ba4da80d0c9ae99ab0722
Артём Алексеев <m2207365@edu.misis.ru>
2024-02-16 00:37:57 +0300

    Made class for Participanits and normative check

--- lab№6_Alekseev/Program.cs
+++ lab№6_Alekseev/Program.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace lab_6_Alekseev
 {
-    internal class Program
+    public class Participant
     {
-        static void Main(string[] args)
+        public string Surname { get; set; }
+        public string Teacher { get; set; }
+        public string Group { get; set; }
+        public double Result { get; set; } //Time in sec
+        public bool Normative { get; private set; }
+
+        public Participant(string surname, string teacher, string group, double result)
         {
-            //test
+            Surname = surname;
+            Teacher = teacher;
+            Group = group;
+            Result = result;
+            CheakNormative();
+        }
+        private void CheakNormative()
+        {
+            Normative = Result <= 2 * 60; // Норматив для юноши 1-го курса на оцеку 3 равен 2 минутам
+        }
+        public static void Main()
+        {
+   
         }
     }
 }
