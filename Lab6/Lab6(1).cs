2dfc4fc4816c3a40f02f30362ed790237683875c
Camrock <m2310094@edu.misis.ru>
2024-02-26 19:57:10 +0300

    Lab6

--- Lab6/Lab6(1).cs
+++ Lab6/Lab6(1).cs
@@ -1,8 +1,4 @@
-﻿/*оценки по информатике, а также определено количество пропущенных занятий.
-Успеваемость каждого студента оценивается следующими баллами: «0»
-(неаттестован), «2», «3», «4» или «5». Вывести список неуспевающих (оценка «2»)
-студентов в порядке убывания количества пропущенных ими занятий.*/
-using System;
+﻿using System;
 
 struct Student
 {
@@ -23,27 +19,34 @@ struct Student
             this.mark = mark;
         }
     }
+
+    public void PrintInfo()
+    {
+        if (Mark == 2)
+        {
+            Console.WriteLine($"Имя: {Name} Кол-во пропусков: {Miss}");
+        }
+    }
 }
 
 class Program
 {
-    
     static void Main()
     {
-        Student[] human = new Student[5];
-        human[0] = new Student("Misha", 2, 3);
-        human[1] = new Student("Pasha", 2, 9);
-        human[2] = new Student("Tigran", 2, 6);
-        human[3] = new Student("Sofia", 5, 1);
-        human[4] = new Student("Ivan", 0, 20);
-
-        Array.Sort(human, (x, y) => y.Miss.CompareTo(x.Miss));
-        for (int i = 0; i < human.Length; i++)
+        Student[] students = new Student[]
         {
-            if (human[i].Mark == 2)
-            {
-                Console.WriteLine($"Имя: {human[i].Name} Кол-во пропусков: {human[i].Miss}");
-            }
+            new Student("Misha", 2, 3),
+            new Student("Pasha", 2, 9),
+            new Student("Tigran", 2, 6),
+            new Student("Sofia", 5, 1),
+            new Student("Ivan", 0, 20)
+        };
+
+        Array.Sort(students, (x, y) => y.Miss.CompareTo(x.Miss));
+
+        foreach (var student in students)
+        {
+            student.PrintInfo();
         }
     }
 }
--- Lab6/Lab6(2).cs
+++ Lab6/Lab6(2).cs
@@ -1,58 +1,52 @@
-﻿/*Протокол соревнований по прыжкам в воду содержит список фамилий спортсменов
-и баллы, выставленные 5 судьями по результатам 2 прыжков. Получить итоговый
-протокол, содержащий фамилии и результаты, в порядке занятых спортсменами
-мест по результатам 2 прыжков.*/
+﻿using Internal;
 using System;
-using Internal;
 
 class Program
 {
     struct Person
     {
         private string surname;
-        private int ref1_1, ref1_2, ref2_1, ref2_2, ref3_1, ref3_2, ref4_1, ref4_2, ref5_1, ref5_2;
+        private int[] refs;
 
         public string Surname => surname;
-        public int Ref1_1 => ref1_1;
-        public int Ref1_2 => ref1_2;
-        public int Ref2_1 => ref2_1;
-        public int Ref2_2 => ref2_2;
-        public int Ref3_1 => ref3_1;
-        public int Ref3_2 => ref3_2;
-        public int Ref4_1 => ref4_1;
-        public int Ref4_2 => ref4_2;
-        public int Ref5_1 => ref5_1;
-        public int Ref5_2 => ref5_2;
-        public int Final_result => ref1_1 + ref1_2 + ref2_1 + ref2_2 + ref3_1 + ref3_2 + ref4_1 + ref4_2 + ref5_1 + ref5_2;
 
-        public Person(string surname, int ref1_1, int ref1_2, int ref2_1, int ref2_2, int ref3_1, int ref3_2, int ref4_1, int ref4_2, int ref5_1, int ref5_2)
+        public int[] Refs => refs;
+
+        public Person(string surname, int[] refs)
         {
             this.surname = surname;
-            this.ref1_1 = ref1_1;
-            this.ref1_2 = ref1_2;
-            this.ref2_1 = ref2_1;
-            this.ref2_2 = ref2_2;
-            this.ref3_1 = ref3_1;
-            this.ref3_2 = ref3_2;
-            this.ref4_1 = ref4_1;
-            this.ref4_2 = ref4_2;
-            this.ref5_1 = ref5_1;
-            this.ref5_2 = ref5_2;
+            this.refs = refs;
+        }
+
+        public int CalculateFinalResult()
+        {
+            int sum = 0;
+            foreach (int reference in refs)
+            {
+                sum += reference;
+            }
+            return sum;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Фамилия: {Surname} Результат: {CalculateFinalResult()}");
         }
     }
+
     static void Main()
     {
         Person[] sportik = new Person[5];
-        sportik[0] = new Person("Oganezov", 4, 5, 2, 3, 4, 3, 4, 4, 5, 5);
-        sportik[1] = new Person("Popov", 3, 5, 1, 4, 2, 5, 3, 3, 2, 2);
-        sportik[2] = new Person("Machkalyan", 1, 2, 1, 1, 3, 3, 4, 4, 2, 4);
-        sportik[3] = new Person("Gubeeva", 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
-        sportik[4] = new Person("Kapelina", 4, 5, 5, 4, 4, 5, 5, 4, 4, 5);
+        sportik[0] = new Person("Oganezov", new int[] { 4, 5, 2, 3, 4, 3, 4, 4, 5, 5 });
+        sportik[1] = new Person("Popov", new int[] { 3, 5, 1, 4, 2, 5, 3, 3, 2, 2 });
+        sportik[2] = new Person("Machkalyan", new int[] { 1, 2, 1, 1, 3, 3, 4, 4, 2, 4 });
+        sportik[3] = new Person("Gubeeva", new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 });
+        sportik[4] = new Person("Kapelina", new int[] { 4, 5, 5, 4, 4, 5, 5, 4, 4, 5 });
 
-        Array.Sort(sportik, (x, y) => y.Final_result.CompareTo(x.Final_result));
+        Array.Sort(sportik, (x, y) => y.CalculateFinalResult().CompareTo(x.CalculateFinalResult()));
         for (int i = 0; i < sportik.Length; i++)
         {
-            Console.WriteLine($"Фамилия: {sportik[i].Surname} Результат: {sportik[i].Final_result}");
+            sportik[i].PrintInfo();
         }
     }
 }
