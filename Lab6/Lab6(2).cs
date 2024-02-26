167b34951b1884084e14182d2059bd03919c9886
Camrock <m2310094@edu.misis.ru>
2024-02-26 09:36:05 +0300

    Lab6

--- Lab6/Lab6(2).cs
+++ Lab6/Lab6(2).cs
@@ -3,34 +3,32 @@
 протокол, содержащий фамилии и результаты, в порядке занятых спортсменами
 мест по результатам 2 прыжков.*/
 using System;
-using Internal;
-
-struct Person
-{
-    private string surname;
-    private int result1, result2, result3, result4, result5;
-
-    public string Surname => surname;
-    public int Result1 => result1;
-    public int Result2 => result2;
-    public int Result3 => result3;
-    public int Result4 => result4;
-    public int Result5 => result5;
-    public int Final_result => Result1 + Result2 + Result3 + Result4 + Result5;
-
-    public Person(string surname, int result1, int result2, int result3, int result4, int result5)
-    {
-        this.surname = surname;
-        this.result1 = result1;
-        this.result2 = result2;
-        this.result3 = result3;
-        this.result4 = result4;
-        this.result5 = result5;
-    }
-}
 
 class Program
 {
+    struct Person
+    {
+        private string surname;
+        private int result1, result2, result3, result4, result5;
+
+        public string Surname => surname;
+        public int Result1 => result1;
+        public int Result2 => result2;
+        public int Result3 => result3;
+        public int Result4 => result4;
+        public int Result5 => result5;
+        public int Final_result => Result1 + Result2 + Result3 + Result4 + Result5;
+
+        public Person(string surname, int result1, int result2, int result3, int result4, int result5)
+        {
+            this.surname = surname;
+            this.result1 = result1;
+            this.result2 = result2;
+            this.result3 = result3;
+            this.result4 = result4;
+            this.result5 = result5;
+        }
+    }
     static void Main()
     {
         Person[] sportik = new Person[5];
