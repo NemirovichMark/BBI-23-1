ae07f1a35fafa6fe6770b8677bd496c0e94af99f
Артём Алексеев <m2207365@edu.misis.ru>
2024-03-18 22:10:18 +0300

    Переделал первый уровень

--- lab№6_Alekseev/Program.cs
+++ lab№6_Alekseev/Program.cs
@@ -6,30 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lab_6_Alekseev
-{
-    public class Participant
-    {
-        public string Surname { get; set; }
-        public string Teacher { get; set; }
-        public string Group { get; set; }
-        public double Result { get; set; } //Time in sec
-        public bool Normative { get; private set; }
 
-        public Participant(string surname, string teacher, string group, double result)
-        {
-            Surname = surname;
-            Teacher = teacher;
-            Group = group;
-            Result = result;
-            CheakNormative();
-        }
-        private void CheakNormative()
-        {
-            Normative = Result <= 2 * 60; // Норматив для юноши 1-го курса на оцеку 3 равен 2 минутам
-        }
-        public static void Main()
-        {
+ 
+
    
-        }
-    }
-}
