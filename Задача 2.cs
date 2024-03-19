32af16457b48708c6835dcab419d37f76f47c602
SophiaChaykina <159889042+SophiaChaykina@users.noreply.github.com>
2024-03-19 12:37:39 +0300

    Add files via upload

--- Задача 2.cs
+++ Задача 2.cs
@@ -1,5 +1,6 @@
 ﻿//Вариант 1, задание 2
 
+using System;
 
 namespace ConsoleApp1
 {
@@ -13,6 +14,7 @@ namespace ConsoleApp1
             _Name = Name;
             _Bonus = Bpnus;
         }
+        public string name { get { return _Name; } }
         public virtual void Print()
         {
             System.Console.WriteLine(($"Название: {_Name}\t Надбавка: {_Bonus}\t"));
@@ -70,6 +72,21 @@ namespace ConsoleApp1
     }
     internal class Program
     {
+        static void Sort(Professdion[] ARRAY)
+        {
+            for(int i=1;i<ARRAY.Length;i++)
+            {
+                for(int j=1;j<ARRAY.Length;j++)
+                {
+                    if (ARRAY[j - 1].Money() < ARRAY[j].Money())
+                    {
+                        Professdion p = ARRAY[j];
+                        ARRAY[j] = ARRAY[j - 1];
+                        ARRAY[j-1] = p;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Fireman[] array1 = new Fireman[5];
@@ -78,6 +95,7 @@ namespace ConsoleApp1
             array1[2] = new Fireman("Пожарный2", 13, 10);
             array1[3] = new Fireman("Пожарный3", 14, 10);
             array1[4] = new Fireman("Пожарный4", 15, 10);
+            Sort(array1);
             for (int i = 0; i < array1.Length; i++)
             {
                 array1[i].Print();
@@ -90,6 +108,7 @@ namespace ConsoleApp1
             array2[2] = new Scientist("Ученый2", 13, 10);
             array2[3] = new Scientist("Ученый3", 15, 10);
             array2[4] = new Scientist("Ученый4", 16, 10);
+            Sort(array2);
             for (int i = 0; i < array2.Length; i++)
             {
                 array2[i].Print();
@@ -102,11 +121,37 @@ namespace ConsoleApp1
             array3[2] = new Engineer("Инжинер2", 19, 10);
             array3[3] = new Engineer("Инжинер3", 14, 10);
             array3[4] = new Engineer("Инжинер4", 12, 10);
+            Sort(array3);
             for (int i = 0; i < array3.Length; i++)
             {
                 array3[i].Print();
             }
 
+            Console.WriteLine();
+            Console.WriteLine("ОбЪединенный массив");
+            int size = array1.Length + array2.Length + array3.Length;
+            Professdion[] array4 = new Professdion[size];
+            int g = 0;
+            for (int i=0;i<array1.Length;i++)
+            {
+                array4[i] = array1[i];
+                g = i;
+            }
+            for (int i = 0; i < (array1.Length); i++)
+            {
+                array4[g] = array2[i];
+                g++;
+            }
+            for (int i = 0; i < (array1.Length); i++)
+            {
+                array4[g] = array3[i];
+                g++;
+            }
+
+            for(int i=0;i<array4.Length;i++)
+            {
+                array4[i].Print();
+            }
 
         }
     }
