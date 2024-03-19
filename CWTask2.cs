d4818591c590a8b6395e7069eebcad72808fcde9
Nadezhda-Titova <146069626+Nadezhda-Titova@users.noreply.github.com>
2024-03-19 12:05:21 +0300

    CWTask2 Вариант 7

---
+++ CWTask2.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+//Вариант 7
+abstract class Goods
+{
+    int article;
+    int quantity;
+    string name;
+}
+
+class Product : Goods
+{
+
+}
+
+class Equipment : Goods
+{
+
+}
+
+class Tool : Goods
+{
+
+}
