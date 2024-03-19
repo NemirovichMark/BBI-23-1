bc7d092206270e851072037d04dce4c5d7a04b5b
SophiaChaykina <159889042+SophiaChaykina@users.noreply.github.com>
2024-03-19 12:12:37 +0300

    Add files via upload

---
+++ Задача 1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public struct Point
+    {
+        private int _x;
+        private int _y;
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+        public static int Rasstoyanie(int X,  int Y)
+        {
+            
+            return(Math.Abs(X - Y));
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"x: {_x}\t y:{_y}\t расстояние: {Rasstoyanie(X,Y)}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] array = new Point[3];
+            array[0] = new Point(1, 4);//3
+            array[1] = new Point(3, 3);//0
+            array[2] = new Point(5, 1);//3
+
+            for(int i=0;i<array.Length;i++)
+            {
+                array[i].Print();
+            }
+        }
+    }
+}
