204479455b1aaab69c5703d19ef18f887e2ec7c0
Oganezov Mikhail <146069423+Camrock@users.noreply.github.com>
2024-04-02 11:25:36 +0300

    Update Lab7(2).cs

--- Lab7/Lab7(2).cs
+++ Lab7/Lab7(2).cs
@@ -1,55 +1,73 @@
 using System;
 
+// Создание абстрактного класса Diving
 abstract class Diving
 {
+    // Защищенное поле disciplineName
     protected string disciplineName;
 
+    // Абстрактное свойство DisciplineName
     public abstract string DisciplineName { get; }
 
+    // Конструктор класса Diving
     public Diving(string disciplineName)
     {
         this.disciplineName = disciplineName;
     }
 
+    // Абстрактный метод PrintDisciplineInfo
     public abstract void PrintDisciplineInfo();
 }
 
+// Класс DivingWith3M наследует класс Diving
 class DivingWith3M : Diving
 {
+    // Переопределенное свойство DisciplineName
     public override string DisciplineName => "Прыжки в воду с 3 метровой вышки";
 
+    // Конструктор класса DivingWith3M
     public DivingWith3M(string disciplineName) : base(disciplineName) { }
 
+    // Переопределенный метод PrintDisciplineInfo
     public override void PrintDisciplineInfo()
     {
         Console.WriteLine($"Дисциплина: {DisciplineName}");
     }
 }
 
+// Класс DivingWith5M наследует класс Diving
 class DivingWith5M : Diving
 {
+    // Переопределенное свойство DisciplineName
     public override string DisciplineName => "Прыжки в воду с 5 метровой вышки";
 
+    // Конструктор класса DivingWith5M
     public DivingWith5M(string disciplineName) : base(disciplineName) { }
 
+    // Переопределенный метод PrintDisciplineInfo
     public override void PrintDisciplineInfo()
     {
         Console.WriteLine($"Дисциплина: {DisciplineName}");
     }
 }
 
+// Основной класс программы
 class Program
 {
+    // Структура Person
     struct Person
     {
+        // Приватные поля surname, refs и diving
         private string surname;
         private int[] refs;
         private Diving diving;
 
+        // Публичные свойства Surname, Refs и Diving
         public string Surname => surname;
         public int[] Refs => refs;
         public Diving Diving => diving;
 
+        // Конструктор структуры Person
         public Person(string surname, int[] refs, Diving diving)
         {
             this.surname = surname;
@@ -57,6 +75,7 @@ class Program
             this.diving = diving;
         }
 
+        // Метод CalculateFinalResult для вычисления суммарного результата
         public int CalculateFinalResult()
         {
             int sum = 0;
@@ -67,6 +86,7 @@ class Program
             return sum;
         }
 
+        // Метод PrintInfo для вывода информации о спортсмене
         public void PrintInfo()
         {
             Console.WriteLine($"Фамилия: {Surname} Результат: {CalculateFinalResult()}");
@@ -74,6 +94,7 @@ class Program
         }
     }
 
+    // Главный метод программы
     static void Main()
     {
         // Создание объектов для спортсменов с присвоением атрибутов прыжков в воду
@@ -83,6 +104,7 @@ class Program
         Diving diving4 = new DivingWith5M("Дисциплина 4");
         Diving diving5 = new DivingWith3M("Дисциплина 5");
 
+        // Создание массива спортсменов
         Person[] sportik = new Person[5];
         sportik[0] = new Person("Oganezov", new int[] { 4, 5, 2, 3, 4, 3, 4, 4, 5, 5 }, diving1);
         sportik[1] = new Person("Popov", new int[] { 3, 5, 1, 4, 2, 5, 3, 3, 2, 2 }, diving2);
@@ -90,7 +112,10 @@ class Program
         sportik[3] = new Person("Gubeeva", new int[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }, diving4);
         sportik[4] = new Person("Kapelina", new int[] { 4, 5, 5, 4, 4, 5, 5, 4, 4, 5 }, diving5);
 
+        // Сортировка массива спортсменов по убыванию результатов
         Array.Sort(sportik, (x, y) => y.CalculateFinalResult().CompareTo(x.CalculateFinalResult()));
+
+        // Вывод информации о спортсменах
         for (int i = 0; i < sportik.Length; i++)
         {
             sportik[i].PrintInfo();
