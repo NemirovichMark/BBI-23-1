be279554461cc5227af92eadc4deb9f20c10de54
Артём Алексеев <m2207365@edu.misis.ru>
2024-04-16 20:08:30 +0300

    Done

--- lvl3/Program.cs
+++ lvl3/Program.cs
@@ -8,8 +8,20 @@ namespace lvl3
 {
     public class Team
     {
-        public string Name { get; set; }
-        public int Points { get; set; }
+        private string _name;
+        private int _points;
+
+
+        public Team(string name, int points)
+        {
+            _name = name;
+            _points = points;
+        }
+
+        public string Name { get { return _name; } }
+
+        public int Points { get { return _points; } }
+
     }
 
     public class Program
@@ -18,57 +30,101 @@ namespace lvl3
         {
             Team[] groupA = new Team[]
             {
-            new Team { Name = "Team A1", Points = 10 },
-            new Team { Name = "Team A2", Points = 9 },
-            new Team { Name = "Team A3", Points = 3},
-            new Team { Name = "Team A4", Points = 10},
-            new Team { Name = "Team A5", Points = 11},
-            new Team { Name = "Team A6", Points = 12 },
-            new Team { Name = "Team A7", Points = 9 },
-            new Team { Name = "Team A8", Points = 8 },
-            new Team { Name = "Team A9", Points = 5 },
-            new Team { Name = "Team A10", Points = 3 },
-            new Team { Name = "Team A11", Points = 4},
-            new Team { Name = "Team A12", Points = 11},
-
-
+            new Team ( "Team A1", 10 ),
+            new Team ( "Team A2", 9 ),
+            new Team ( "Team A3", 3 ),
+            new Team ( "Team A4", 10 ),
+            new Team ( "Team A5", 11 ),
+            new Team ( "Team A6", 12 ),
+            new Team ( "Team A7", 9 ),
+            new Team ( "Team A8", 8 ),
+            new Team ( "Team A9", 5 ),
+            new Team ( "Team A10", 3 ),
+            new Team ( "Team A11", 4 ),
+            new Team ( "Team A12" ,11 ),
             };
 
 
             Team[] groupB = new Team[]
             {
-            new Team { Name = "Team B1", Points = 12 },
-            new Team { Name = "Team B2", Points = 6 },
-            new Team { Name = "Team B3", Points = 3 },
-            new Team { Name = "Team B4", Points = 3 },
-            new Team { Name = "Team B5", Points = 12 },
-            new Team { Name = "Team B6", Points = 10},
-            new Team { Name = "Team B7", Points = 9},
-            new Team { Name = "Team B8", Points = 11 },
-            new Team { Name = "Team B9", Points = 5 },
-            new Team { Name = "Team B10", Points = 3 },
-            new Team { Name = "Team B11", Points = 1 },
-            new Team { Name = "Team B12", Points = 8 },
-
+            new Team ( "Team B1", 12 ),
+            new Team ( "Team B2", 6 ),
+            new Team ( "Team B3", 3 ),
+            new Team ( "Team B4", 3 ),
+            new Team ( "Team B5", 12 ),
+            new Team ( "Team B6", 10 ),
+            new Team ( "Team B7", 9 ),
+            new Team ( "Team B8", 11 ),
+            new Team ( "Team B9",  5 ),
+            new Team ( "Team B10", 3 ),
+            new Team ( "Team B11", 1 ),
+            new Team ( "Team B12", 8 ),
             };
 
 
-            Team[] topTeamsGroupA = SelectTopTeams(groupA);
-            Team[] topTeamsGroupB = SelectTopTeams(groupB);
+            Sort(groupB);
+            Sort(groupA);
 
-            Team[] secondStageTeams = topTeamsGroupA.Concat(topTeamsGroupB).ToArray();
 
-            Console.WriteLine("Команды участники во второй стадии:");
-            foreach (var team in secondStageTeams)
+            Team[] groupC = MergeArrays(groupA, groupB);
+
+            foreach (var team in groupC)
             {
                 Console.WriteLine(team.Name);
             }
             Console.ReadKey();
         }
 
-        public static Team[] SelectTopTeams(Team[] teams)
+
+
+        static Team[] MergeArrays(Team[] array1, Team[] array2)
         {
-            return teams.OrderByDescending(t => t.Points).Take(6).ToArray();
+            int n1 = array1.Length;
+            int n2 = array2.Length;
+            Team[] mergedArray = new Team[n1 + n2];
+            int i = 0, j = 0, k = 0;
+
+            while (i < n1 && j < n2 && k < 12)
+            {
+                if (array1[i].Points >= array2[j].Points)
+                {
+                    mergedArray[k++] = array1[i++];
+                }
+                else
+                {
+                    mergedArray[k++] = array2[j++];
+                }
+            }
+
+            while (i < n1 && k < 12)
+            {
+                mergedArray[k++] = array1[i++];
+            }
+
+            while (j < n2 && k < 12)
+            {
+                mergedArray[k++] = array2[j++];
+            }
+
+            return mergedArray;
+        }
+
+
+
+        static void Sort(Team[] teams)
+        {
+            for (int i = 0; i < teams.Length - 1; i++)
+            {
+                for (int j = 0; j < teams.Length - i - 1; j++)
+                {
+                    if (teams[j].Points < teams[j + 1].Points)
+                    {
+                        Team temp = teams[j];
+                        teams[j] = teams[j + 1];
+                        teams[j + 1] = temp;
+                    }
+                }
+            }
         }
     }
 }
