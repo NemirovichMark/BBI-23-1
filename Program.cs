305452f70c7b261fc968b4a40961c05fd56cb624
AgapovaEkaterina <146069418+AgapovaEkaterina@users.noreply.github.com>
2024-04-23 11:59:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,325 +1,127 @@
-﻿# region 1_4
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
 
-//public class Program
-//{
-//    struct Ychastnik
-//    {
-//        private string name;
-//        private int id;
-//        private int _bestres;
-
-//        public int Result { get { return _bestres; } }
-
-//        public Ychastnik(string name, int i, int bestres)
-//        {
-//            this.name = name;
-//            id = i;
-//            _bestres = bestres;
-//        }
-
-//        public int ressec
-//        {
-//            get { return _bestres; }
-//            set
-//            {
-//                if (value > _bestres) _bestres = value;
-//            }
-//        }
-
-//        public void Info()
-//        {
-//            Console.WriteLine($"результат: {_bestres} имя участника: {name} ");
-//        }
-
-//    }
-
-//    static Ychastnik[] Sort(Ychastnik[] list)
-//    {
-//        for (int i = 0; i < list.Length; i++)
-//        {
-//            for (int j = i; j < list.Length; j++)
-//                if (list[i].Result < list[j].Result)
-//                {
-//                    Ychastnik person_now = list[j];
-//                    list[j] = list[i]; 
-//                    list[i] = person_now; 
-//                }
-//        }
-//        return list;
-//    }
-
-
-//    public static void Main(string[] args)
-//    {
-//        int n = int.Parse(Console.ReadLine());
-//        Ychastnik[] peoplelist = new Ychastnik[n];
-
-//        for (int i = 0; i < n; i++)
-//        {
-//            string name_people = Console.ReadLine();
-//            int firstres = int.Parse(Console.ReadLine());
-//            int secondres = int.Parse(Console.ReadLine());
-
-//            Ychastnik Person = new Ychastnik(name_people, i + 1, firstres);
-//            Person.ressec = secondres;
-
-//            peoplelist[i] = Person;
-//        }
-
-//        peoplelist = Sort(peoplelist);
-
-//        for (int i = 0; i < n; i++)
-//        {
-//            Console.Write($"{i + 1} занятое место:"); ;
-//            peoplelist[i].Info();
-//            Console.WriteLine();
-//        }
-//        Console.WriteLine();
-//        Console.ReadLine();
-//    }
-//}
-#endregion
-
-# region 2_4
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-
-//struct Sportsman
-//{
-//    private string name;
-//    private double jump1;
-//    private double jump2;
-//    private double jump3;
-//    private double jump4;
-
-//    public Sportsman(string name, double one, double two, double three, double four)
-//    {
-//        this.name = name;
-//        jump1 = one;
-//        jump2 = two;
-//        jump3 = three;
-//        jump4 = four;
-//    }
-
-
-//    public double endresult()
-//    {
-//        return jump1 + jump2 + jump3 + jump4;
-//    }
-
-//    public void Info()
-//    {
-//        Console.WriteLine($"результат: {jump1 + jump2 + jump3 + jump4} имя: {name} ");
-//    }
-//}
-
-//public class Program
-//{
-
-//    public static int[] SortedGolosa(int[] list, int n)
-//    {
-//        for (int i = 1; i < n; i++)
-//        {
-//            for (int j = 0; j < n - i; j++)
-//            {
-//                if (list[j + 1] > list[j])
-//                {
-//                    int now = list[j];
-//                    list[j] = list[j + 1];
-//                    list[j + 1] = now;
-//                }
-//            }
-//        }
-//        return list;
-//    }
-
-//    static Sportsman[] Sort(Sportsman[] list)
-//    {
-//        for (int i = 0; i < list.Length; i++)
-//        {
-//            for (int j = i; j < list.Length; j++)
-//                if (list[i].endresult() < list[j].endresult())
-//                {
-//                    Sportsman pers = list[j]; 
-//                    list[j] = list[i]; 
-//                    list[i] = pers; 
-//                }
-//        }
-//        return list;
-//    }
-
-
-//    public static void Main(string[] args)
-//    {
-//        int n = int.Parse(Console.ReadLine());
-//        Sportsman[] peoplelist = new Sportsman[n];
-
-//        for (int i = 0; i < n; i++)
-//        {
-//            string name_people = Console.ReadLine();
-//            double[] resjumps = new double[4];
-//            for (int j = 0; j < 4; j++) 
-//            {
-//                double koef = double.Parse(Console.ReadLine()); 
-//                int[] golosa = new int[7]; 
-//                for (int s = 0; s < 7; s++)
-//                {
-//                    int golos = int.Parse(Console.ReadLine());
-//                    golosa[s] = golos;
-//                }
-
-//                golosa = SortedGolosa(golosa, 7); 
-//                int sum_golos = 0;
-//                for (int k = 1; k < 6; k++)
-//                {
-//                    sum_golos += golosa[k];
-//                }
-
-//                resjumps[j] = sum_golos * koef; 
-//            }
-
-//            Sportsman pers = new Sportsman(name_people, resjumps[0], resjumps[1], resjumps[2], resjumps[3]);
-//            peoplelist[i] = pers;
-//        }
-
-//        peoplelist = Sort(peoplelist);
-
-//        for (int i = 0; i < n; i++)
-//        {
-//            Console.Write($"{i + 1} место:"); ;
-//            peoplelist[i].Info();
-//            Console.WriteLine();
-//        }
-//        Console.WriteLine();
-//        Console.ReadLine();
-//    }
-//}
-#endregion
-
-#region 3_4
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-public class Program
+abstract class Task
 {
-    struct Ychastnik
+    protected string text;
+    public Task(string text)
     {
-        private string name;
-        private int result;
+        this.text = text;
+    }
+    protected abstract void ParseText(string text);
+}
 
-        public int Result { get { return result; } }
-
-        public Ychastnik(string name, int res)
+class Task_1 : Task
+{
+    public Task_1(string text) : base(text)
+    {
+        ParseText(text);
+    }
+    public override string ToString()
+    {
+        return text;
+    }
+    private string Find_long(string text)
+    {
+        string[] symb = new string[] { ".", "!", "?" };
+        for (int i = 0; i < symb.Length; i++)
         {
-            this.name = name;
-            result = res;
+            text = text.Replace(symb[i], "&&");
         }
-
-        public void Info()
+        text += " ";
+        string[] lines = text.Split("&& ");
+        string line = "";
+        int longline = 0;
+        for (int j = 0; j < lines.Length; j++)
         {
-            Console.WriteLine($"результат: {result} имя: {name} ");
+            if (lines[j].Length > longline)
+            {
+                line = lines[j];
+                longline = lines[j].Length;
+            }
         }
+        return line;
 
     }
 
-    static Ychastnik[] Sort(Ychastnik[] list)
+    protected override void ParseText(string text)
     {
-        for (int i = 0; i < list.Length; i++)
-        {
-            for (int j = i; j < list.Length; j++)
-                if (list[i].Result < list[j].Result)
-                {
-                    Ychastnik person_now = list[j]; 
-                    list[j] = list[i]; 
-                    list[i] = person_now;
-                }
-        }
-        return list;
-    }
-
-
-    public static void Main(string[] args)
-    {
-        int n1 = int.Parse(Console.ReadLine());
-        int n2 = int.Parse(Console.ReadLine());
-        Ychastnik[] onegr = new Ychastnik[n1];
-        Ychastnik[] twogr = new Ychastnik[n2];
-        Ychastnik[] allgr = new Ychastnik[n2 + n1];
-
-        for (int i = 0; i < n1; i++)
-        {
-            string name_ = Console.ReadLine();
-            int result_ = int.Parse(Console.ReadLine());
-
-            Ychastnik Person = new Ychastnik(name_, result_);
-
-            onegr[i] = Person;
-            allgr[i] = Person;
-        }
-
-        for (int i = 0; i < n2; i++)
-        {
-            string name_ = Console.ReadLine();
-            int result_ = int.Parse(Console.ReadLine());
-
-            Ychastnik Person = new Ychastnik(name_, result_);
-
-            twogr[i] = Person;
-            allgr[i + n1] = Person;
-        }
-
-        onegr = Sort(onegr);
-        twogr = Sort(twogr);
-        allgr = Sort(allgr);
-
-        Console.WriteLine("1 группа:");
-        for (int i = 0; i < n1; i++)
-        {
-            Console.Write($"{i + 1} место:"); ;
-            onegr[i].Info();
-            Console.WriteLine();
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("2 группа:");
-        for (int i = 0; i < n2; i++)
-        {
-            Console.Write($"{i + 1} место:"); ;
-            twogr[i].Info();
-            Console.WriteLine();
-        }
-        Console.WriteLine();
-
-        Console.WriteLine("все участники:");
-        for (int i = 0; i < n1 + n2; i++)
-        {
-            Console.Write($"{i + 1} место:"); ;
-            allgr[i].Info();
-            Console.WriteLine();
-        }
-        Console.WriteLine();
-        Console.ReadLine();
+        this.text = Find_long(text);
     }
 }
-#endregion
 
+class Program
+{
+    public static void Main()
+    {
+        Task_1 task1 = new Task_1("Привет! я самое длинное предложение. а ты?");
+        Console.WriteLine(task1);
 
+    }
+}
+////
+class Task_2 : Task
+{
+    public Task_2(string text) : base(text)
+    {
+        ParseText(text);
+    }
+    public override string ToString()
+    {
+        return text;
+    }
+    private char Find_word(string text)
+    {
+        string[] words = text.Split(" ");
+        char[] best = new char[words.Length];
+        int[] count = new int[words.Length];
+        int idd = 0;
+        for (int j = 0; j < words.Length; j++)
+        {
+            bool f = false;
+            for (int i = 0; i < best.Length; i++)
+            {
+                if (words[j][0] == best[i])
+                {
+                    count[i] += 1;
+                    f = true;
+                    break;
+                }
+            }
+            if (!f)
+            {
+                best[idd] = words[j][0];
+                count[idd] = 1;
+                idd++;
+            }
+        }
 
+        int l = 0;
+        char sym = '-';
+        for (int j = 0; j <= count.Length; j++)
+        {
+            if (count[j] > l)
+            {
+                l = count[j];
+                sym = best[j];
+            }
+        }
+        return sym;
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    protected override void ParseText(string text)
+    {
+        char n = Find_word(text);
+        string s = "";
+        string[] m = text.Split(" ");
+        for (int i = 0; i < m.Length
+          ; i++)
+        {
+            if (m[i][0] == n)
+            {
+                s += m[i] + " ";
+            }
+        }
+        this.text = s;
+    }
+}
