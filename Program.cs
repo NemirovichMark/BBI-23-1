074cd7f82965a9e5b20e8a0c9f91647d96429dfe
RomanIbramhalilov <146069497+RomanIbramhalilov@users.noreply.github.com>
2024-05-01 20:54:43 +0300

    8 лаба

--- Program.cs
+++ Program.cs
@@ -1,135 +1,297 @@
 ﻿using System;
+using System.Collections.Generic;
 
-abstract class Runner
+// Абстрактный класс "Задание"
+public abstract class Zadanie
 {
-    protected string _surname;
-    protected internal double _result;
-    protected string _coach;
-    protected int _group;
+    protected string text;
 
-    public Runner(string surname, double result, string coach, int group)
+    public Zadanie(string text)
     {
-        _surname = surname;
-        _result = result;
-        _coach = coach;
-        _group = group;
+        this.text = text;
     }
 
-    public abstract void DisplayInfo();
+    public abstract void Reshit();
+    public override abstract string ToString();
+}
 
-    
-    public static void Qsort(FootballTeam[] a, int low, int high)
+// Первое задание (№1)
+public class ChastotaBukv : Zadanie
+{
+    private int[] chastota = new int[33];
+
+    public ChastotaBukv(string text) : base(text) { }
+
+    public override void Reshit()
     {
-        if (low < high)
+
+        int totalBukv = 0;
+
+        foreach (char c in text.ToLower())
         {
-            int pi = Prtition(a, low, high);
-
-            Qsort(a, low, pi - 1);
-            Qsort(a, pi + 1, high);
-        }
-    }
-
-    private static int Prtition(FootballTeam[] a, int low, int high)
-    {
-        int pivot = a[high].Ball;
-        int i = low - 1;
-
-        for (int j = low; j < high; j++)
-        {
-            if (a[j].Ball > pivot)
+            if (char.IsLetter(c))
             {
-                i++;
-                FootballTeam temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                chastota[c - 'а']++;
+                totalBukv++;
             }
         }
 
-        FootballTeam temp2 = a[i + 1];
-        a[i + 1] = a[high];
-        a[high] = temp2;
-
-        return i + 1;
-    }
-}
-
-class FootballTeam
-{
-    public string ClubName { get; protected set; }
-    public int Ball { get; protected set; }
-
-    public FootballTeam(string clubName, int points)
-    {
-        ClubName = clubName;
-        Ball = points;
-    }
-
-    public virtual void DisplayInfo()
-    {
-        Console.WriteLine($"{ClubName} {this.GetType().Name}: {Ball} points");
-    }
-}
-
-class WomenFootballTeam : FootballTeam
-{
-    public WomenFootballTeam(string clubName, int points) : base(clubName, points)
-    {
-    }
-}
-
-class MenFootballTeam : FootballTeam
-{
-    public MenFootballTeam(string clubName, int points) : base(clubName, points)
-    {
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        FootballTeam[] womenClubs = new FootballTeam[]
+        for (int i = 0; i < chastota.Length; i++)
         {
-            new WomenFootballTeam("Spartak", 15),
-            new WomenFootballTeam("Zenit", 12),
-            new WomenFootballTeam("Loko", 18),
-            new WomenFootballTeam("CSKA", 20),
-            new WomenFootballTeam("Dynamo", 16),
-            new WomenFootballTeam("Torpedo", 14)
-        };
-
-        FootballTeam[] menClubs = new FootballTeam[]
-        {
-            new MenFootballTeam("Anzhi", 22),
-            new MenFootballTeam("Ural", 19),
-            new MenFootballTeam("Rubin", 17),
-            new MenFootballTeam("Arsenal", 21),
-            new MenFootballTeam("Fakel", 16),
-            new MenFootballTeam("Krasnodar", 18)
-        };
-
-        FootballTeam[] combClubs = new FootballTeam[12];
-        int i = 0;
-
-        foreach (FootballTeam club in womenClubs)
-        {
-            combClubs[i] = club;
-            i++;
+            chastota[i] = (int)((double)chastota[i] / totalBukv * 100);
         }
 
-        foreach (FootballTeam club in menClubs)
+
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = 0; i < 33; i++)
         {
-            combClubs[i] = club;
-            i++;
+            char bukva = (char)('а' + i);
+            result += $"{bukva}: {chastota[i]}% ";
+        }
+        return result;
+    }
+
+}
+
+
+
+// Второе задание (№3)
+public class RazbienieNaStroki : Zadanie
+{
+    private List<string> stroki = new List<string>();
+    public RazbienieNaStroki(string text) : base(text) { }
+
+    public override void Reshit()
+    {
+        string[] slova = text.Split(' ');
+        string stroka = "";
+
+
+        foreach (string slovo in slova)
+        {
+            if (stroka.Length + slovo.Length + 1 <= 50)
+            {
+                stroka += slovo + " ";
+            }
+            else
+            {
+                stroki.Add(stroka.Trim());
+                stroka = slovo + " ";
+            }
+        }
+        stroki.Add(stroka.Trim());
+
+
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        foreach (string stroka in stroki)
+        {
+            result += stroka + "\n";
+        }
+        return result;
+    }
+}
+
+
+
+// 3 задание (№5)
+public class ChastotaNachalnyhBukv : Zadanie
+{
+    private int[] chastota = new int[33];
+    public ChastotaNachalnyhBukv(string text) : base(text) { }
+
+    public override void Reshit()
+    {
+
+        string[] slova = text.Split(' ', '.', ',', '!', '?', ':', ';');
+
+        foreach (string slovo in slova)
+        {
+            if (slovo.Length > 0)
+            {
+                char firstChar = char.ToLower(slovo[0]);
+                if (char.IsLetter(firstChar))
+                {
+                    chastota[firstChar - 'а']++;
+                }
+            }
         }
 
-        Console.WriteLine("Top teams:");
-        Runner.Qsort(combClubs, 0, combClubs.Length - 1);
 
-        for (int j = 0; j < combClubs.Length; j++)
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = 0; i < 33; i++)
         {
-            Console.WriteLine($"{j + 1}. ");
-            combClubs[j].DisplayInfo();
+            if (chastota[i] > 0)
+            {
+                char bukva = (char)('а' + i);
+                result += $"{bukva} ";
+            }
         }
+        return result;
+    }
+}
+//4 задние(№7)
+public class PoiskOdnokorennyhSlov : Zadanie
+{
+    private string koren;
+    List<string> naĭdennyeSlova = new List<string>();
+    public PoiskOdnokorennyhSlov(string text, string koren) : base(text)
+    {
+        this.koren = koren.ToLower();
+
+    }
+    public override void Reshit()
+    {
+        string[] slova = text.ToLower().Split(' ', '.', ',', '!', '?', ':', ';');
+
+
+        foreach (string slovo in slova)
+        {
+            if (IsOdnokorennoe(slovo, koren))
+            {
+                naĭdennyeSlova.Add(slovo);
+            }
+        }
+
+
+    }
+
+    private bool IsOdnokorennoe(string slovo, string koren)
+    {
+
+        return slovo.StartsWith(koren) && slovo.Length > koren.Length;
+    }
+
+
+
+
+    public override string ToString()
+    {
+        string result = "Найденные однокоренные слова: ";
+        foreach (string slovo in naĭdennyeSlova)
+        {
+            result += slovo + " ";
+        }
+        return result;
+    }
+}
+
+//5 задание (№14)
+public class SummaChisel : Zadanie
+{
+    private int summa;
+
+    public SummaChisel(string text) : base(text) { }
+
+    public override void Reshit()
+    {
+        summa = 0;
+
+
+        foreach (char symbol in text)
+        {
+
+            if (symbol >= '0' && symbol <= '9')
+            {
+
+                summa += symbol - '0';
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Сумма цифр в тексте: {summa}";
+    }
+}
+
+//6 задание (#11)
+public class SortirovkaFamilij : Zadanie
+{
+    private string[] Items;
+
+    public SortirovkaFamilij(string text) : base(text) { }
+
+    public override void Reshit()
+    {
+
+        Items = text.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < Items.Length - 1; i++)
+        {
+            for (int j = 0; j < Items.Length - i - 1; j++)
+            {
+                if (string.Compare(Items[j], Items[j + 1], StringComparison.CurrentCultureIgnoreCase) > 0)
+                {
+                    string temp = Items[j];
+                    Items[j] = Items[j + 1];
+                    Items[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return string.Join(", ", Items);
+    }
+}
+
+// Пример использования
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        string text = "международных инвесторов и кредиторов. Последствия дефолта оказались глубокими и долгосрочными: сокращение кредитного рейтинга страны, увеличение затрат на заемный капитал, рост стоимости заимствований и утрата доверия со стороны международных инвесторов. ";
+        string koren = "вод";
+        Console.WriteLine("Первое");
+        Zadanie zadanie1 = new ChastotaBukv(text);
+        zadanie1.Reshit();
+        Console.WriteLine(zadanie1);
+
+        Console.WriteLine("Второе");
+        Zadanie zadanie2 = new RazbienieNaStroki(text);
+        zadanie2.Reshit();
+        Console.WriteLine(zadanie2);
+
+        Console.WriteLine("Третье");
+        Zadanie zadanie3 = new ChastotaNachalnyhBukv(text);
+        zadanie3.Reshit();
+        Console.WriteLine(zadanie3);
+
+
+        string text1 = "Водопад это вода.Водная гладь прекрасна";
+        Console.WriteLine("Четвертое");
+        Zadanie zadanie4 = new PoiskOdnokorennyhSlov(text1, koren);
+        zadanie4.Reshit();
+        Console.WriteLine(zadanie4);
+
+
+
+        Console.WriteLine("Пятое");
+
+
+        string input = "Текст содержит 1 слово, 2015 числа и 3 знака препинания. Сумма: 999 !";
+        Zadanie zadanie6 = new SummaChisel(input);
+        zadanie6.Reshit();
+        Console.WriteLine(zadanie6);
+
+        Console.WriteLine("Шестое");
+        string input5 = "Козлов\r\nНовиков\r\nИванова\r\nПетрова\r\nСмирнова\r\n\r\n\r\nIvanov\r\nPetrov\r\nSmirnov\r\nSokolov\r\n, Антонов Борисов";
+        Zadanie task5 = new SortirovkaFamilij(input5);
+        task5.Reshit();
+        Console.WriteLine(task5.ToString());
     }
 }
