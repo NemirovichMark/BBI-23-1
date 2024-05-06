6b7ac2da808dc72b0e68ba9880ddcaba22ddb5ee
alekseevart3m <159888933+alekseevart3m@users.noreply.github.com>
2024-05-07 01:20:27 +0300

    Lab8_task14

---
+++ Lab8_task14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_task14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Вселенная, в которой мы живём, имеет возраст около 4 миллиардов лет. Солнце, наша звезда, имеет диаметр около 3 миллиона километров. Расстояние от Земли до Солнца составляет 7 миллионов километров.";
+            int counter = 0;
+            string[] words = text.Split(' ');
+            foreach (string word in words)
+            {
+                if ( int.TryParse(word, out int number) )
+                {
+                    counter = +number;
+                }
+            }
+            Console.WriteLine(counter);
+            Console.ReadKey();
+        }
+    }
+}
