4b243896a546b399963bef96205f9138919866e3
PopovaPolina005 <146069915+PopovaPolina005@users.noreply.github.com>
2024-05-17 10:30:42 +0300

    Add files via upload

---
+++ XmlMySerializer.cs
@@ -0,0 +1,26 @@
+﻿
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab9_3_4.Serializers
+{
+    class XmlMySerializer : MySerializer
+    {
+        public override void Write<T>(T obj, string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                new XmlSerializer(typeof(T)).Serialize(fs, obj);
+            }
+        }
+        public override T Read<T>(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                return (T)new XmlSerializer(typeof(T)).Deserialize(fs);
+            }
+        }
+    }
+}
+
+
