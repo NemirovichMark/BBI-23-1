af41400fcf6039f434bc0ba0d514da9f13bfc9bc
Polina-Hrishchanovich <146069635+Polina-Hrishchanovich@users.noreply.github.com>
2024-05-21 11:22:12 +0300

    KR-1 №1

--- Program.cs
+++ Program.cs
@@ -1,143 +1,62 @@
-﻿#region 2_1
-/*
+﻿
 using System;
-class Program
+struct Point
 {
-    struct Student
+    private double x;
+    private double y;
+
+    public Point(double[] coordinates)
     {
-        private string famile;
-        private double mark;
-
-        public Student(string f, int mark1, int mark2, int mark3, int mark4)
-        {
-            famile = f;
-            mark = (double)(mark1 + mark2 + mark3 + mark4) / 4;
-
-        }
-        public void Print()
-        {
-            Console.WriteLine($" {famile}\tбалл  {mark}");
-        }
-
-
-        public string Famile
-        {
-            get { return famile; }
-        }
-
-        public double Mark
-        {
-            get { return mark; }
-        }
+        x = coordinates[0];
+        y = coordinates[1];
     }
 
-    static void Main()
+    public double X
     {
-        Student[] st = new Student[5];
-        st[0] = new Student("студент1", 5, 5, 2, 5);
-        st[1] = new Student("студент2", 5, 4, 5, 5);
-        st[2] = new Student("студент3", 3, 2, 3, 4);
-        st[3] = new Student("студент4", 4, 2, 5, 5);
-        st[4] = new Student("студент5", 3, 5, 4, 3);
+        get { return x; }
+    }
 
-        for (int i = 0; i < st.Length; i++)
-        {
-            for (int j = 0; j < st.Length - 1; j++)
-            {
-                if (st[j].Mark < st[j + 1].Mark)
-                {
-                    (st[j], st[j + 1]) = (st[j + 1], st[j]);
-                }
-            }
-        }
+    public double Y
+    {
+        get { return y; }
+    }
 
-        Console.WriteLine();
-        Console.WriteLine(" Студенты ");
-        for (int i = 0; i < st.Length; i++)
-        {
-            if (st[i].Mark >= 4)
-            {
-                st[i].Print();
-            }
-        }
+    public static double Distance(Point p1, Point p2)
+    {
+        return Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+    }
+
+    public static void PrintInfo(Point p1, Point p2)
+    {
+        double distance = Distance(p1, p2);
+        Console.WriteLine($"Точка 1: ({p1.X}, {p1.Y})");
+        Console.WriteLine($"Точка 2: ({p2.X}, {p2.Y})");
+
+        Console.WriteLine($"Расстояние между точками: {distance:F2}");
     }
 }
-*/
-#endregion
 
-#region 3_5
-/*
-struct FootballTeam
-{
-    private string name;
-    private int Scored;
-    private int Failed; 
-    private int points;
-    public FootballTeam(string name)
-    {
-        this.name = name;
-        Scored = 0; 
-        Failed = 0;
-        points = 0;
-    }
-    public string Name { get { return name; } set { name = value; } }
-    public void Result(int scored, int conceded)
-    {
-        Scored += scored;
-        Failed += conceded; 
-        if (scored > conceded) points += 3;
-        else if (scored == conceded) points += 1;
-    }
-    public int Points { get { return points; } }
-    public int Difference { get { return Scored - Failed; } }
-    public static void Print(FootballTeam[] teams)
-    {
-        Console.WriteLine("  Place      Team          Points");
-        for (int i = 0; i < teams.Length; i++)
-        {
-            Console.WriteLine("{0,5}     {1,-6}      {2} ", i + 1, teams[i].Name, teams[i].Points);
-        }
-    }
-}
 class Program
 {
     static void Main(string[] args)
     {
-        FootballTeam[] teams = new FootballTeam[]        
+        Point[] points = new Point[]
         {
-            new FootballTeam("Барселона"),            
-            new FootballTeam("Реал Мадрид"),
-            new FootballTeam("Дерби Каунти")        
+            new Point(new double[] { 1.0, 2.0 }),
+            new Point(new double[] { 3.0, 4.0 }),
+            new Point(new double[] { -1.0, 5.0 })
         };
-        Match(ref teams[0], ref teams[1]);
-        Match(ref teams[0], ref teams[2]);
-        Match(ref teams[1], ref teams[0]);
-        Match(ref teams[1], ref teams[2]);
-        Sort(teams); FootballTeam.Print(teams);
-    }
-    static void Match(ref FootballTeam team1, ref FootballTeam team2)
-    {
-        Random random = new Random(); 
-        int scored = random.Next(0, 5);
-        int conceded = random.Next(0, 5);
-        team1.Result(scored, conceded);
-        team2.Result(conceded, scored);
-    }
-    static void Sort(FootballTeam[] teams)
-    {
-        int n = teams.Length;
-        for (int i = 0; i < n - 1; i++)
+
+        Console.WriteLine("Информация о точках:");
+
+        for (int i = 0; i < points.Length; i++)
         {
-            for (int j = 0; j < n - i - 1; j++)
+            for (int j = i + 1; j < points.Length; j++)
             {
-                if (teams[j].Points < teams[j + 1].Points || (teams[j].Points == teams[j + 1].Points && teams[j].Difference < teams[j + 1].Difference))
-                {
-                    FootballTeam temp = teams[j];
-                    teams[j] = teams[j + 1]; teams[j + 1] = temp;
-                }
+                Console.WriteLine($"Пара точек {i + 1} и {j + 1}:");
+                Point.PrintInfo(points[i], points[j]);
+                Console.WriteLine();
             }
         }
     }
 }
-*/
-#endregion
