30fe731afdbcf30571d873d7fc29272aa5fb5d1a
KarinaAbramenkova <146069361+KarinaAbramenkova@users.noreply.github.com>
2024-05-28 12:15:01 +0300

    Add files via upload

---
+++ кр 2.cs
@@ -0,0 +1,28 @@
+﻿//вариант 15 Абраменкова Карина
+//1 
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string input = Console.ReadLine();
+
+        int count = CountConjunctionsAndPrepositions(input);
+
+        Console.WriteLine("Количество предлогов и союзов без гласных букв: " + count);
+    }
+
+    static int CountConjunctionsAndPrepositions(string input)
+    {
+        string[] conjunctionsAndPrepositions = { "и", "так", "да", "но", "а", "что", "чтобы", "если", "либо", "поэтому", "потому" };
+
+        string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        int count = words.Count(word => conjunctionsAndPrepositions.Contains(word.ToLower()) && !word.Any(c => "аеёиоуыэюя".Contains(c)));
+
+        return count;
+    }
+}
