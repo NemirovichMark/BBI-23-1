e1f4935353d1dfb16ecb6f0e20f271dd25b863a4
Polina-Hrishchanovich <146069635+Polina-Hrishchanovich@users.noreply.github.com>
2024-05-28 12:17:57 +0300

    Защита 8 лабы

---
+++ laba8 защита.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+abstract class Task
+{
+    public string Text { get; set; }
+
+    public Task(string text)
+    {
+        Text = text;
+    }
+
+    public abstract List<string> ParseText();
+}
+
+class Task8 : Task
+{
+    public List<string> lines = new List<string>();
+
+    public Task8(string text) : base(text)
+    {
+        ParseText();
+    }
+
+    public override List<string> ParseText()
+    {
+        string[] words = Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string word in words)
+        {
+            lines.Add(word.PadLeft(50));
+        }
+        return lines;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string text = "1 июля 2015 года Греция объявила о дефолте по государственному долгу, став первой развитой страной в истории. Высокопревосходительство";
+
+        Task8 task8 = new Task8(text);
+        Console.WriteLine("Задание 8");
+        Console.WriteLine("\n\tКаждое слово с новой строки :D :");
+        foreach (var line in task8.lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
