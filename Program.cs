2aa2d86b1aed370e88ba88afba162bf07004bea4
KuularAmina <146070177+KuularAmina@users.noreply.github.com>
2024-05-28 12:02:22 +0300

    защита 6 лаба

--- Program.cs
+++ Program.cs
@@ -1,143 +1,158 @@
 ﻿using System;
-    class Team
+
+class Team
+{
+    public string Name { get; private set; }
+    public int Win { get; private set; }
+    public int Lose { get; private set; }
+    public int Draw { get; private set; }
+    public int Goal { get; private set; }
+    public int Miss { get; private set; }
+    public int Score { get; private set; }
+
+    public Team(string Name)
     {
-        public string Name { get; set; }
-        public int Win { get; private set; }
-        public int Lose { get; private set; }
-        public int Draw { get; private set; }
-        public int Goal { get; private set; }
-        public int Miss { get; private set; }
-        public int Score { get; private set; }
-
-        public Team(string Name)
-        {
-            this.Name = Name;
-            this.Win = 0;
-            this.Lose = 0;
-            this.Draw = 0;
-            this.Goal = 0;
-            this.Miss = 0;
-            this.Score = 0;
-        }
-
-        public static bool operator ==(Team A, Team B)
-        {
-            return A.Name == B.Name;
-        }
-
-        public static bool operator !=(Team A, Team B)
-        {
-            return A.Name != B.Name;
-        }
-
-        public void SetScore(int a, int b)
-        {
-            int A = 0, B = 0, C = 0;
-            if (a > b) A++;
-            if (a < b) B++;
-            if (a == b) C++;
-
-            this.Win += A;
-            this.Lose += B;
-            this.Draw += C;
-            this.Goal += a;
-            this.Miss += b;
-            this.Score += A * 3 + C * 1;
-        }
-
+        this.Name = Name;
+        this.Win = 0;
+        this.Lose = 0;
+        this.Draw = 0;
+        this.Goal = 0;
+        this.Miss = 0;
+        this.Score = 0;
     }
 
-    class Match
+    public static bool operator ==(Team A, Team B)
     {
-        public Team[] T { get; set; }
-        public int n { get; set; }
+        return A.Name == B.Name;
+    }
 
-        public Match()
+    public static bool operator !=(Team A, Team B)
+    {
+        return A.Name != B.Name;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Team other = (Team)obj;
+        return Name == other.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+
+    public void SetScore(int a, int b)
+    {
+        int A = 0, B = 0, C = 0;
+        if (a > b) A++;
+        if (a < b) B++;
+        if (a == b) C++;
+
+        this.Win += A;
+        this.Lose += B;
+        this.Draw += C;
+        this.Goal += a;
+        this.Miss += b;
+        this.Score += A * 3 + C * 1;
+    }
+}
+
+class Match
+{
+    public Team[] T { get; private set; }
+    public int n { get; private set; }
+
+    public Match()
+    {
+        this.T = new Team[100];
+        this.n = 0;
+    }
+
+    public void Add(string NameA, string NameB, int a, int b)
+    {
+        bool f = false;
+        for (int i = 0; i < n; i++)
         {
-            this.T = new Team[100];
-            this.n = 0;
+            if (T[i].Name == NameA)
+            {
+                T[i].SetScore(a, b);
+                f = true;
+                break;
+            }
+        }
+        if (!f)
+        {
+            T[n] = new Team(NameA);
+            T[n].SetScore(a, b);
+            n++;
         }
 
-        public void Add(string NameA, string NameB, int a, int b)
+        f = false;
+        for (int i = 0; i < n; i++)
         {
-            bool f = false;
-            for (int i = 0; i < n; i++)
+            if (T[i].Name == NameB)
             {
-                if (T[i].Name == NameA)
-                {
-                    T[i].SetScore(a, b);
-                    f = true;
-                    break;
-                }
-            }
-            if (!f)
-            {
-                T[n] = new Team(NameA);
-                T[n].SetScore(a, b);
-                n++;
-            }
-
-            f = false;
-            for (int i = 0; i < n; i++)
-            {
-                if (T[i].Name == NameB)
-                {
-                    T[i].SetScore(b, a);
-                    f = true;
-                    break;
-                }
-            }
-            if (!f)
-            {
-                T[n] = new Team(NameB);
-                T[n].SetScore(b, a);
-                n++;
+                T[i].SetScore(b, a);
+                f = true;
+                break;
             }
         }
-
-        public void Sort()
+        if (!f)
         {
-            for (int i = 0; i < n - 1; i++)
-            {
-                double amax = T[i].Score;
-                int imax = i;
-                for (int j = i + 1; j < n; j++)
-                {
-                    if (T[j].Score > amax) 
-                    {
-                        amax = T[j].Score;
-                        imax = j;
-                    }
-                }
-                Team temp;
-                temp = T[imax];
-                T[imax] = T[i];
-                T[i] = temp;
-            }
+            T[n] = new Team(NameB);
+            T[n].SetScore(b, a);
+            n++;
         }
     }
 
-
-    class MainClass
+    public void Sort()
     {
-        public static void Main(string[] args)
+        GnomeSort(T, n);
+    }
+
+    private void GnomeSort(Team[] arr, int length)
+    {
+        int index = 0;
+
+        while (index < length)
         {
-            Match M = new Match();
-            M.Add("Медведи", "Динамо", 3, 0);
-            M.Add("Зенит", "Море", 4, 1);
-            M.Add("Восход", "Алмаз", 0, 2);
-            M.Add("Медведи", "Алмаз", 2, 3);
-            M.Add("Зенит", "Динамо", 5, 1);
-            M.Add("Море", "Восход", 2, 1);
-            M.Add("Зенит", "Восход", 2, 3);
-
-            M.Sort();
-
-            for (int i = 0; i < M.n; i++)
+            if (index == 0 || arr[index].Score <= arr[index - 1].Score)
             {
-                Console.WriteLine(i + 1 + ". " + M.T[i].Name + ": " + M.T[i].Score);
+                index++;
+            }
+            else
+            {
+                Team temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
             }
         }
     }
+}
 
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Match M = new Match();
+        M.Add("Медведи", "Динамо", 3, 0);
+        M.Add("Зенит", "Море", 4, 1);
+        M.Add("Восход", "Алмаз", 0, 2);
+        M.Add("Медведи", "Алмаз", 2, 3);
+        M.Add("Зенит", "Динамо", 5, 1);
+        M.Add("Море", "Восход", 2, 1);
+        M.Add("Зенит", "Восход", 2, 3);
 
+        M.Sort();
+
+        for (int i = 0; i < M.n; i++)
+        {
+            Console.WriteLine(i + 1 + ". " + M.T[i].Name + ": " + M.T[i].Score);
+        }
+    }
+}
