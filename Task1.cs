d6f28075e9aa4a391027422bf732b5f7ad07654e
TaranovArtem <146069426+TaranovArtem@users.noreply.github.com>
2024-06-18 11:50:57 +0300

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant_1
+{
+    public class Task1
+    {
+        //struct Number
+        public struct Number
+        {
+            //field
+            private double real;
+
+            //property to read a field
+            public double Real
+            {
+                get
+                {
+                    return real;
+                }
+            }
+
+            //constructor
+            public Number(double realNum)
+            {
+                this.real = realNum;
+            }
+
+            //method to add numbers
+            public Number Add(Number num)
+            {
+                return new Number(this.real + num.real);
+            }
+
+            //method to subtract numbers
+            public Number Sub(Number other)
+            {
+                return new Number(this.real - other.real);
+            }
+
+            //method to multiply numbers
+            public Number Mul(Number other)
+            {
+                return new Number(this.real * other.real);
+            }
+
+            //method to divide numbers
+            public Number Div(Number num)
+            {
+                if (num.real != 0)
+                    return new Number(this.real / num.real);
+                else
+                    return new Number(0);
+            }
+
+            //output
+            public override string ToString()
+            {
+                return $"Number = {this.real}";
+            }
+        }
+
+        //stuff particularly for class Task1
+
+        //field
+        private Number[] numbers;
+
+        //property to read a field
+        public Number[] Numbers
+        {
+            get { return numbers; }
+        }
+
+        //constructor
+        public Task1(Number[] number)
+        {
+            this.numbers = number;
+        }
+
+        //method to sort array with numbers in ascending order
+        public void Sorting() //insertion sort
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                Number current = numbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j].Real > current.Real)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = current;
+            }
+        }
+
+        //method to output the array with numbers 
+        public override string ToString()
+        {
+            string result = "";
+            
+            foreach (var number in numbers)
+            {
+                Console.WriteLine();
+                result +=  number.ToString();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task3.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant_1
+{
+    public class Task3
+    {
+        public class Searcher
+        {
+            //fields
+            private string input;
+            private string[] output;
+
+            //public properties to read fields
+            public string Input
+            {
+                get { return input; }
+            }
+            public string[] Output
+            {
+                get { return output; }
+            }
+
+            //constructor
+            public Searcher(string text)
+            {
+                input = text;
+                output = ProcessText(text);
+            }
+
+            //method to turn a text into array of strings
+            public string[] ProcessText(string text)
+            {
+                var words = text.Split(new[] { ' ', '.', ',', '!', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var repeatedWords = new List<string>();
+                var checkedWords = new List<string>(); 
+
+                //check words repetitions
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (!checkedWords.Contains(words[i]))
+                    {
+                        int count = 0;
+                        for (int j = 0; j < words.Length; j++)
+                        {
+                            if (words[i] == words[j])
+                            {
+                                count++;
+                            }
+                        }
+
+                        if (count > 1) //if we meet a word in a text more than once we add it to an output with repeated words
+                        {
+                            repeatedWords.Add(words[i]);
+                        }
+
+                        checkedWords.Add(words[i]); //not to check again
+                    }
+                }
+
+                return repeatedWords.ToArray();
+            }
+
+            //output
+            public override string ToString()
+            {
+                return string.Join(" ", output);
+            }
+        }
+    }
+}
